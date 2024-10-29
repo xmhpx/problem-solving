6438ab3e5d62ab946b81c44e17151b0b8cb01bb1
xmhpx <mhppaydar@gmail.com>
2024-10-29 06:56:31 +0330

    added Fenwick andd LPsolver

---
+++ FenwickTree.cpp
@@ -0,0 +1,88 @@
+#include <vector>
+
+using namespace std;
+
+struct FenwickTree {
+    vector<int> bit;  // binary indexed tree
+    int n;
+
+    FenwickTree(int n) {
+        this->n = n;
+        bit.assign(n, 0);
+    }
+
+    FenwickTree(vector<int> const &a) : FenwickTree(a.size()) {
+        for (size_t i = 0; i < a.size(); i++)
+            add(i, a[i]);
+    }
+
+    int sum(int r) {
+        int ret = 0;
+        for (; r >= 0; r = (r & (r + 1)) - 1)
+            ret += bit[r];
+        return ret;
+    }
+
+    int sum(int l, int r) {
+        return sum(r) - sum(l - 1);
+    }
+
+    void add(int idx, int delta) {
+        for (; idx < n; idx = idx | (idx + 1))
+            bit[idx] += delta;
+    }
+};
+
+struct FenwickTreeOneBasedIndexing {
+    vector<int> bit;  // binary indexed tree
+    int n;
+
+    FenwickTreeOneBasedIndexing(int n) {
+        this->n = n + 1;
+        bit.assign(n + 1, 0);
+    }
+
+    FenwickTreeOneBasedIndexing(vector<int> a)
+        : FenwickTreeOneBasedIndexing(a.size()) {
+        for (size_t i = 0; i < a.size(); i++)
+            add(i, a[i]);
+    }
+
+    int sum(int idx) {
+        int ret = 0;
+        for (++idx; idx > 0; idx -= idx & -idx)
+            ret += bit[idx];
+        return ret;
+    }
+
+    int sum(int l, int r) {
+        return sum(r) - sum(l - 1);
+    }
+
+    void add(int idx, int delta) {
+        for (++idx; idx < n; idx += idx & -idx)
+            bit[idx] += delta;
+    }
+};
+
+struct FenwickTree2D {
+    vector<vector<int>> bit;
+    int n, m;
+
+    // init(...) { ... }
+
+    int sum(int x, int y) {
+        int ret = 0;
+        for (int i = x; i >= 0; i = (i & (i + 1)) - 1)
+            for (int j = y; j >= 0; j = (j & (j + 1)) - 1)
+                ret += bit[i][j];
+        return ret;
+    }
+
+    void add(int x, int y, int delta) {
+        for (int i = x; i < n; i = i | (i + 1))
+            for (int j = y; j < m; j = j | (j + 1))
+                bit[i][j] += delta;
+    }
+};
+
--- SegmentTree.cpp
+++ SegmentTree.cpp
@@ -28,3 +28,68 @@ class MaxSegTree {
 		return max_;
 	}
 };
+
+
+struct node{
+    node* cl{};
+    node* cr{};
+    long long la{0};
+	long long mx{0};
+    int l, r;
+
+    node(int _l, int _r) : l(_l), r(_r) {}
+
+	inline void merge(){
+		mx = max(cl->mx, cr->mx);
+	}
+
+	inline void push(){
+		cl->la += la;
+		cr->la += la;
+		cl->mx += la;
+		cr->mx += la;
+		la = 0;
+	}
+
+    long long ask(int po){
+        if(l == r){
+            return mx;
+        }
+
+        int m = (l+r)/2;
+        if(!cl){
+            cl = new node(l, m);
+        }
+        if(!cr){
+            cr = new node(m+1, r);
+        }
+		push();
+        if(po <= m){
+            return cl->ask(po);
+        }
+        else{
+            return cr->ask(po);
+        }
+    }
+
+    void add(int ql, int qr, int vl){
+        if(qr < ql)return;
+        if(qr < l || r < ql)return;
+        if(ql <= l && r <= qr){
+            la += vl;
+            return;
+        }
+
+        int m = (l+r)/2;
+        if(!cl){
+            cl = new node(l, m);
+        }
+        if(!cr){
+            cr = new node(m+1, r);
+        }
+		push();
+        cl->add(ql, qr, vl);
+        cr->add(ql, qr, vl);
+		merge();
+    }
+};
